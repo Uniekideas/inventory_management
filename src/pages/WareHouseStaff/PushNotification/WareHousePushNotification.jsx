0197db6924e94df0039e0271f95d48ba376d688f
Obinna Ukpabi <askphntm@gmail.com>
2024-10-10 13:49:10 +0100

    Update WareHousePushNotification.jsx

--- src/pages/WareHouseStaff/PushNotification/WareHousePushNotification.jsx
+++ src/pages/WareHouseStaff/PushNotification/WareHousePushNotification.jsx
@@ -15,7 +15,7 @@ import NotificationBtn from "../../../components/Button/NotificationBtn";
 
 function WareHousePushNotification() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-  const [audience, setAudience] = useState('')
+  const [audience, setAudience] = useState("");
 
   const [showModal, setShowModal] = useState(false);
 
@@ -42,57 +42,47 @@ function WareHousePushNotification() {
       pk: 4,
       type: "Admin",
     },
-    
   ];
   return (
     <div>
       <NavigationHeader toggleSidebar={toggleSidebar} />
       <div className="d-flex justify-content-between">
-      <WareHouseSideNavigation isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+        <WareHouseSideNavigation
+          isOpen={isSidebarOpen}
+          toggleSidebar={toggleSidebar}
+        />
         <Container className="reportContainer">
-        <BackButtonIcon/> 
+          <BackButtonIcon />
           <TitleHeader text={"Push Notifications"} />
 
           <Row>
+            <Row className="d-lg-none">
+              <Col className="d-flex text-center justify-content-end mb-3">
+                <NotificationBtn
+                  Primaryicon={faClockRotateLeft}
+                  onClick={handleShow}
+                  text={"Notification History"}
+                  Primarystyle={"pushNotificationTimer "}
+                />
+              </Col>
+            </Row>
+            <NotificationHistory show={showModal} handleClose={handleClose} />
 
-          <Row className="d-lg-none">
-            <Col className="d-flex text-center justify-content-end mb-3">
+            <Col className="d-flex justify-content-end ms-auto gap-3 mb-5">
               <NotificationBtn
                 Primaryicon={faClockRotateLeft}
                 onClick={handleShow}
                 text={"Notification History"}
-                Primarystyle={"pushNotificationTimer "}
+                Primarystyle={"pushNotificationTimer d-none d-lg-flex"}
               />
-              
             </Col>
+            <NotificationHistory show={showModal} handleClose={handleClose} />
           </Row>
-            <NotificationHistory   show={showModal} handleClose={handleClose} />
-
-            <Col className="d-flex justify-content-end ms-auto gap-3 mb-5">
-              <Filter
-                optionTitle={"Select Target Audience:"}
-                options={filterData}
-                defult={"All"}
-                onSelect={(value)=>setAudience(value)}
-
-              />
-
-              <NotificationBtn
-              Primaryicon={faClockRotateLeft}
-              onClick={handleShow}
-              text={"Notification History"}
-              Primarystyle={"pushNotificationTimer d-none d-lg-flex"}
-            />
-
-           
-            </Col>
-            <NotificationHistory   show={showModal} handleClose={handleClose} />
-          </Row>
-          <PushNotification audience={audience}/>
+          <PushNotification audience={audience} />
         </Container>
       </div>
     </div>
   );
 }
 
-export default  WareHousePushNotification;
+export default WareHousePushNotification;
